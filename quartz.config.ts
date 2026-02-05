2a0d5035bcef38b14be5c90344194fb76e269447
Ivan <iviaramburu@gmail.com>
2026-02-05 13:06:22 -0300

    Fix baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "github.com/Ivan-I-Ar/Eire",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
